7c28fce103ec6713d7f7a66ae9e0843d8519f1f3
Karl Pettersson <karl_pettersson_1998@yahoo.com>
2016-10-04 19:16:14 +0200

    Updated to work with Julia v0.5

--- src/LifeTable.jl
+++ src/LifeTable.jl
@@ -23,7 +23,7 @@ function PeriodLifeTable(inframe, sex, openend=true, intype="count")
 	else
 		ifirst = age[2:nrows] .- age[1:nrows-1]
 		iend = ifirst[nrows-1]
-		i = [ifirst, iend]
+		i = [ifirst; iend]
 	end
 
 	if intype == "count"
@@ -48,26 +48,26 @@ function PeriodLifeTable(inframe, sex, openend=true, intype="count")
 	if openend
 		aend = 1 / m[nrows]
 		aoth = fill(0.5, nrows-2)
-		aclosed = [a0, aoth]
-		a = [aclosed, aend]
+		aclosed = [a0; aoth]
+		a = [aclosed; aend]
 		qclosed = map(q, m[1:nrows-1], aclosed, i)
-		q = [qclosed, 1]
+		q = [qclosed; 1]
 	else
 		aoth = fill(0.5, nrows-1)
-		a = [a0, aoth]
+		a = [a0; aoth]
 		q = map(q, m, a, i)
 	end
 
 	p = 1 .- q
-	l = cumprod([1, p[1:nrows-1]])
+	l = cumprod([1; p[1:nrows-1]])
 	d = q .* l
 
 	if openend 
 		ldclosed = map(ld, l[1:nrows-1], aclosed, d[1:nrows-1], i)
-		ld = [ldclosed, l[nrows]*aend]
+		ld = [ldclosed; l[nrows]*aend]
 		revl = reverse(ld)
 		tclosed = reverse(cumsum(revl))[1:nrows-1]
-		t = [tclosed, ld[nrows]]
+		t = [tclosed; ld[nrows]]
 	else
 		ld = map(ld, l, a, d, i)
 		revl = reverse(ld)
@@ -94,35 +94,48 @@ function CauseLife(lifetable, causefreq)
 	return outframe
 end
 
+gomprate(x,p) = p[1] * exp(p[2].*x)
+gompsurv(x,p) = exp(-(p[1]/p[2]) * (exp(p[2].*x)-1))
+weibrate(x,p) = (p[1]/p[2]) .* (x./p[2]).^(p[1]-1)
+weibsurv(x,p) = exp(-(x./p[2]).^p[1])
+
+models = Dict(
+	"gompertz" => Dict("rate" => gomprate, "surv" => gompsurv, 
+		"p" => [exp(-18); 0.1]),
+	"weibull" => Dict("rate" => weibrate, "surv" => weibsurv,
+		"p" => [10.0; 80.0]),
+	)
+
 function MortSurvFit(lifetable, numbdeaths, func, functype)
-	if func == "gompertz"
-		ratemodel(x, p) = p[1] * exp(p[2].*x)
-		survmodel(x, p) = exp(-(p[1]/p[2]) * (exp(p[2].*x)-1))
-		p = [exp(-18), 0.1]
-	elseif func == "weibull"
-		ratemodel(x, p) = (p[1]/p[2]) .* (x./p[2]).^(p[1]-1)
-		survmodel(x, p) = exp(-(x./p[2]).^p[1]) 
-		p = [10.0, 80.0]
-	end
-	
-	if functype == "rate"
-		ycol = lifetable[:m]
-		model = ratemodel
-	elseif functype == "surv"
-		ycol = lifetable[:l]
-		model = survmodel
-	end
+	ycols = Dict("rate" => :m, "surv" => :l)
 
 	xarr = convert(Array{Int}, lifetable[:age])
-	yarr = convert(Array{Float64}, ycol)
+	yarr = convert(Array{Float64}, lifetable[ycols[functype]])
 	warr = sqrt(convert(Array{Int}, numbdeaths))
+	model = models[func][functype]
 
-	fit = curve_fit(model, xarr, yarr, warr, p)
-	return ["fit" => fit, "func" => func, "functype" => functype, "model" => model]
+	fit = curve_fit(model, xarr, yarr, warr, models[func]["p"])
+	return Dict("fit" => fit, "func" => func, "functype" => functype, "model" => model)
 end
 
 Predict(x, msfit) = msfit["model"](x, msfit["fit"].param)
 
+gompratetr_x(p1, p2) = p2
+gompratetr_y(p1, p2) = log(p1) 
+gompsurvtr_x(p1, p2) = p2
+gompsurvtr_y(p1, p2) = log(p1/p2) 
+weibratetr_x(p1, p2) = p1 - 1
+weibratetr_y(p1, p2) = log(p1/p2) - (p1-1) * log(p2)
+weibsurvtr_x(p1, p2) = p1
+weibsurvtr_y(p1, p2) = -p1 * log(p2) 
+
+transmodels = Dict(
+	"gompertz" =>  Dict("rate" => [gompratetr_x; gompratetr_y],
+		"surv" => [gompsurvtr_x; gompsurvtr_y]),
+	"weibull" =>  Dict("rate" => [weibratetr_x; weibratetr_y],
+		"surv" => [weibsurvtr_x; weibsurvtr_y]),
+	)
+	
 function MortSurvParamsFit(msfits)
 	p1arr = Float64[]
 	p2arr = Float64[]
@@ -134,32 +147,16 @@ function MortSurvParamsFit(msfits)
 	
 	func = msfits[1]["func"]
 	functype = msfits[1]["functype"]
-	if func == "gompertz"
-		ratetrans_x(p1, p2) = p2
-		ratetrans_y(p1, p2) = log(p1)
-		survtrans_x(p1, p2) = p2
-		survtrans_y(p1, p2) = log(p1/p2)
-	elseif func == "weibull"
-		ratetrans_x(p1, p2) = p1 - 1
-		ratetrans_y(p1, p2) = log(p1/p2) - (p1-1) * log(p2)
-		survtrans_x(p1, p2) = p1
-		survtrans_y(p1, p2) = -p1 * log(p2) 
-	end
-	
-	if functype == "rate"
-		trans_x = ratetrans_x
-		trans_y = ratetrans_y
-	elseif functype == "surv"
-		trans_x = survtrans_x
-		trans_y = survtrans_y
-	end
+
+	trans_x = transmodels[func][functype][1]
+	trans_y = transmodels[func][functype][2]
 
 	xarr = map(trans_x, p1arr, p2arr)
 	yarr = map(trans_y, p1arr, p2arr)
 	trans_params = DataFrame(p1 = p1arr, p2 = p2arr, X = xarr, Y = yarr)
 	
 	fit = glm(Y~X, trans_params, Normal(), IdentityLink())
-	return ["trans_params" => trans_params, "fit" => fit]
+	return Dict("trans_params" => trans_params, "fit" => fit)
 end
 
 end
